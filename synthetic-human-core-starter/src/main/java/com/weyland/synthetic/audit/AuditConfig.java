2f8549e62f2d69edab3353b8b536d4f94ce9cfa6
BeloeColeno <semion.semen2013@gmail.com>
2025-07-17 21:42:13 +0500

    Add AuditConfig

---
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/audit/AuditConfig.java
@@ -0,0 +1,32 @@
+package com.weyland.synthetic.audit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AuditConfig {
+    @Value("${weyland.audit.kafka.topic:weyland-audit}")
+    private String auditTopic;
+
+    @Bean
+    @ConditionalOnProperty(name = "weyland.audit.kafka.enabled", havingValue = "true")
+    public KafkaAuditSender kafkaAuditSender(KafkaTemplate<String, String> kafkaTemplate) {
+        return new KafkaAuditSender(auditTopic, kafkaTemplate);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "weyland.audit.kafka.enabled", havingValue = "false", matchIfMissing = true)
+    public KafkaAuditSender consoleAuditSender() {
+        return new KafkaAuditSender("console", null);
+    }
+
+    @Bean
+    public AuditAspect auditAspect(KafkaAuditSender kafkaAuditSender) {
+        return new AuditAspect(kafkaAuditSender);
+    }
+}
