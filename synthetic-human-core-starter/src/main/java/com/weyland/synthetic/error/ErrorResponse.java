846a271c7f02ddf53f81cc7669466ae2e059888d
BeloeColeno <semion.semen2013@gmail.com>
2025-07-17 22:31:07 +0500

    Fix constructor

--- synthetic-human-core-starter/src/main/java/com/weyland/synthetic/error/ErrorResponse.java
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/error/ErrorResponse.java
@@ -1,12 +1,18 @@
 package com.weyland.synthetic.error;
 
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class ErrorResponse {
     String message;
     String code;
     LocalDateTime timestamp;
+
+    public static ErrorResponse of(String message, String code) {
+        return new ErrorResponse(message, code, LocalDateTime.now());
+    }
 }
+
