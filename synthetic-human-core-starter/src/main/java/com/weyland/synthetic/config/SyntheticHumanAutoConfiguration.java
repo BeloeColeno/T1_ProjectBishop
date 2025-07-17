ecc416806c028e55371f522aa4ef77b2c4ef72db
BeloeColeno <semion.semen2013@gmail.com>
2025-07-17 22:29:02 +0500

    Add SyntheticHumanAutoConfiguration in monitoring

---
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/config/SyntheticHumanAutoConfiguration.java
@@ -0,0 +1,53 @@
+package com.weyland.synthetic.config;
+
+import com.weyland.synthetic.audit.AuditAspect;
+import com.weyland.synthetic.audit.KafkaAuditSender;
+import com.weyland.synthetic.command.CommandExecutor;
+import com.weyland.synthetic.monitoring.SyntheticMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+@EnableConfigurationProperties(SyntheticHumanProperties.class)
+@ConditionalOnClass(CommandExecutor.class)
+@EnableAspectJAutoProxy
+public class SyntheticHumanAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CommandExecutor commandExecutor(SyntheticHumanProperties properties) {
+        return new CommandExecutor(
+                properties.getCommand().getCorePoolSize(),
+                properties.getCommand().getMaxPoolSize(),
+                properties.getCommand().getQueueCapacity()
+        );
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public KafkaAuditSender kafkaAuditSender(SyntheticHumanProperties properties, KafkaTemplate<String, String> kafkaTemplate) {
+        return new KafkaAuditSender(properties.getAudit().getKafkaTopic(), kafkaTemplate);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AuditAspect auditAspect(KafkaAuditSender kafkaSender) {
+        return new AuditAspect(kafkaSender);
+    }
+
+    @Bean
+    @ConditionalOnBean(MeterRegistry.class)
+    @ConditionalOnMissingBean
+    public SyntheticMetrics syntheticMetrics(MeterRegistry registry, CommandExecutor commandExecutor) {
+        SyntheticMetrics metrics = new SyntheticMetrics(registry, commandExecutor);
+        metrics.initMetrics();
+        return metrics;
+    }
+}
