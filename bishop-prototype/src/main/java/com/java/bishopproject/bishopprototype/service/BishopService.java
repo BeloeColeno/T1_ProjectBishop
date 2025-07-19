0090207b0781ee8eb436f4864844e74448b5ce69
BeloeColeno <semion.semen2013@gmail.com>
2025-07-19 12:16:55 +0500

    Add BishopService for BishopPrototypeApplication

---
+++ bishop-prototype/src/main/java/com/java/bishopproject/bishopprototype/service/BishopService.java
@@ -0,0 +1,26 @@
+package com.java.bishopproject.bishopprototype.service;
+
+import com.weyland.synthetic.audit.WeylandWatchingYou;
+import com.weyland.synthetic.command.CommandExecutor;
+import com.weyland.synthetic.command.CommandModel;
+import com.weyland.synthetic.monitoring.SyntheticMetrics;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BishopService {
+
+    private final CommandExecutor commandExecutor;
+    private final SyntheticMetrics syntheticMetrics;
+
+    @WeylandWatchingYou(mode = WeylandWatchingYou.AuditMode.CONSOLE)
+    public void processCommand(CommandModel command) {
+        commandExecutor.executeCommand(command);
+        syntheticMetrics.incrementTasksCompletedForAuthor(command.getAuthor());
+    }
+
+    public int getCurrentQueueSize() {
+        return commandExecutor.getCurrentQueueSize();
+    }
+}
