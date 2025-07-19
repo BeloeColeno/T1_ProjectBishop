8d875830b4ad9c9c19aa94d542a441130cb99836
BeloeColeno <semion.semen2013@gmail.com>
2025-07-20 00:34:04 +0500

    Implementation custom validating from starter

--- bishop-prototype/src/main/java/com/java/bishopproject/bishopprototype/controller/CommandController.java
+++ bishop-prototype/src/main/java/com/java/bishopproject/bishopprototype/controller/CommandController.java
@@ -1,12 +1,16 @@
 package com.java.bishopproject.bishopprototype.controller;
 
 import com.java.bishopproject.bishopprototype.service.BishopService;
-import com.weyland.synthetic.audit.WeylandWatchingYou;
 import com.weyland.synthetic.command.CommandModel;
-import jakarta.validation.Valid;
+import com.weyland.synthetic.error.ErrorResponse;
+import com.weyland.synthetic.error.ErrorResponseUtils;
+import com.weyland.synthetic.error.ValidationUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/commands")
@@ -16,13 +20,29 @@ public class CommandController {
     private final BishopService bishopService;
 
     @PostMapping("/console-audit")
-    public ResponseEntity<String> processCommandConsole(@Valid @RequestBody CommandModel command) {
+    public ResponseEntity<?> processCommandConsole(@RequestBody CommandModel command) {
+        // Это кастомная валидация из стартера, можно было бы сделать на @Valid, но я запарился
+        Map<String, String> errors = ValidationUtils.validateCommand(command);
+        if (!errors.isEmpty()) {
+            ErrorResponse errorResponse = ErrorResponseUtils.createValidationErrorResponse(
+                    "Ошибка валидации входных данных", errors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
         bishopService.processCommandConsole(command);
         return ResponseEntity.ok("Команда обработана с аудитом в консоль");
     }
 
     @PostMapping("/kafka-audit")
-    public ResponseEntity<String> processCommandKafka(@Valid @RequestBody CommandModel command) {
+    public ResponseEntity<?> processCommandKafka(@RequestBody CommandModel command) {
+        // Это кастомная валидация из стартера, можно было бы сделать на @Valid, но я запарился
+        Map<String, String> errors = ValidationUtils.validateCommand(command);
+        if (!errors.isEmpty()) {
+            ErrorResponse errorResponse = ErrorResponseUtils.createValidationErrorResponse(
+                    "Ошибка валидации входных данных", errors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
         bishopService.processCommandKafka(command);
         return ResponseEntity.ok("Команда обработана с аудитом в Kafka");
     }
