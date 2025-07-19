a79291685248749adf5b08a9185e1c2adf280e0e
BeloeColeno <semion.semen2013@gmail.com>
2025-07-20 00:31:23 +0500

    Add exception messages for validation

--- synthetic-human-core-starter/src/main/java/com/weyland/synthetic/command/CommandModel.java
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/command/CommandModel.java
@@ -7,18 +7,18 @@ import lombok.Data;
 
 @Data
 public class CommandModel {
-    @NotBlank
-    @Size(max = 1000)
+    @NotBlank(message = "Описание не может быть пустым")
+    @Size(max = 1000, message = "Описание не может превышать 1000 символов")
     private String description;
 
     private CommandPriority priority;
 
-    @NotBlank
-    @Size(max = 100)
+    @NotBlank(message = "Имя автора не может быть пустым")
+    @Size(max = 100, message = "Имя автора не может превышать 100 символов")
     private String author;
 
-    @NotBlank
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @NotBlank(message = "Время не может быть пустым")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", shape = JsonFormat.Shape.STRING)
     private String time;
 
     public enum CommandPriority {
