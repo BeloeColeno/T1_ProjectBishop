eda8a28a7308fd2d6334519035684af776237b69
BeloeColeno <semion.semen2013@gmail.com>
2025-07-19 12:18:16 +0500

    Support annotation for application added (maybe removed later)

--- bishop-prototype/src/main/java/com/java/bishopproject/bishopprototype/BishopPrototypeApplication.java
+++ bishop-prototype/src/main/java/com/java/bishopproject/bishopprototype/BishopPrototypeApplication.java
@@ -1,9 +1,12 @@
 package com.java.bishopproject.bishopprototype;
 
+import com.weyland.synthetic.config.SyntheticHumanAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SyntheticHumanAutoConfiguration.class)
 public class BishopPrototypeApplication {
 
 	public static void main(String[] args) {
