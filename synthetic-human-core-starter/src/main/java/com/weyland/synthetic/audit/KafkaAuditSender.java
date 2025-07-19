1ecce584ae428bb850f72b29487fd0db19c781d2
BeloeColeno <semion.semen2013@gmail.com>
2025-07-20 00:30:18 +0500

    Fix KafkaAuditSender with CompletableFuture

--- synthetic-human-core-starter/src/main/java/com/weyland/synthetic/audit/KafkaAuditSender.java
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/audit/KafkaAuditSender.java
@@ -3,6 +3,9 @@ package com.weyland.synthetic.audit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -12,8 +15,22 @@ public class KafkaAuditSender {
 
     public void sendAuditMessage(String message) {
         if (kafkaTemplate != null) {
-            log.info("Sending audit to Kafka topic [" + topic + "]: " + message);
-            kafkaTemplate.send(topic, message);
+            try {
+                log.info("Sending audit to Kafka topic [" + topic + "]: " + message);
+
+                CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+
+                future.whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        log.debug("Sent message='{}' to topic='{}'", message, topic);
+                    } else {
+                        log.error("Unable to send message='{}' to topic='{}'", message, topic, ex);
+                    }
+                });
+            } catch (Exception e) {
+                log.error("Error while sending audit message to Kafka", e);
+                log.info("[Kafka Error, Fallback Audit:] " + message);
+            }
         } else {
             log.info("[Kafka Unreachable, Audit:] " + message);
         }
