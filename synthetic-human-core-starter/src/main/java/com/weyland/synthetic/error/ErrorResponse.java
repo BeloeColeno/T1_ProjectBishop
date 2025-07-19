624d6b323239e100c216b4dd92628e56184d8c3d
BeloeColeno <semion.semen2013@gmail.com>
2025-07-20 00:23:45 +0500

    Integrating own error handler

--- synthetic-human-core-starter/src/main/java/com/weyland/synthetic/error/ErrorResponse.java
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/error/ErrorResponse.java
@@ -3,16 +3,24 @@ package com.weyland.synthetic.error;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ErrorResponse {
-    String message;
-    String code;
-    LocalDateTime timestamp;
+    private String message;
+    private String code;
+    private LocalDateTime timestamp;
+    private Map<String, String> details;
 
-    public static ErrorResponse of(String message, String code) {
-        return new ErrorResponse(message, code, LocalDateTime.now());
+    public static ErrorResponse of(String message, String code, Map<String, String> details) {
+        return ErrorResponse.builder()
+                .message(message)
+                .code(code)
+                .timestamp(LocalDateTime.now())
+                .details(details)
+                .build();
     }
 }
-
