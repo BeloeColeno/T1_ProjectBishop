3b0893d6524caf4a72c3cf0f7e72e6a44b2b5257
BeloeColeno <semion.semen2013@gmail.com>
2025-07-20 00:27:29 +0500

    Refactor SyntheticMetrics for correct metrics mapping

--- synthetic-human-core-starter/src/main/java/com/weyland/synthetic/monitoring/SyntheticMetrics.java
+++ synthetic-human-core-starter/src/main/java/com/weyland/synthetic/monitoring/SyntheticMetrics.java
@@ -6,6 +6,8 @@ import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
@@ -13,19 +15,36 @@ public class SyntheticMetrics {
     private final MeterRegistry registry;
     private final CommandExecutor commandExecutor;
     private final ConcurrentHashMap<String, Counter> tasksByAuthor = new ConcurrentHashMap<>();
+    private Counter totalTasksCounter;
 
     public void initMetrics() {
         Gauge.builder("synthetic.tasks.queue.size", commandExecutor::getCurrentQueueSize)
                 .description("Current number of tasks in queue")
                 .register(registry);
+
+        totalTasksCounter = Counter.builder("synthetic.tasks.completed")
+                .description("Total number of tasks completed")
+                .register(registry);
     }
 
     public void incrementTasksCompletedForAuthor(String author) {
         tasksByAuthor
-                .computeIfAbsent(author, a -> Counter.builder("synthetic.tasks.completed")
+                .computeIfAbsent(author, a -> Counter.builder("synthetic.tasks.completed.author")
                         .tag("author", author)
                         .description("Number of tasks completed by author")
                         .register(registry))
                 .increment();
+
+        totalTasksCounter.increment();
+    }
+
+    public int getCurrentQueueSize() {
+        return commandExecutor.getCurrentQueueSize();
+    }
+
+    public Map<String, Integer> getTasksCompletedByAuthor() {
+        Map<String, Integer> result = new HashMap<>();
+        tasksByAuthor.forEach((author, counter) -> result.put(author, (int) counter.count()));
+        return result;
     }
 }
